0d9c83e0f3945b47199a5d20dbfd1504e9c00c88
Kolinnnnn <kuba.kolinski11@gmail.com>
2025-01-12 18:47:03 +0100

    added product and category API's

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view()),
     path('user/password-reset/<email>/', userauths_views.PasswordResetEmailVerify.as_view(), name ='password_reset'),
-    path('user/password-change/', userauths_views.PasswordChangeView.as_view(), name='password_change')
+    path('user/password-change/', userauths_views.PasswordChangeView.as_view(), name='password_change'),
+
+    #Store endpoints
+    path('category/', store_views.CategoryListAPIView.as_view()),
+    path('products/', store_views.ProductListAPIView.as_view()),
+    path('products/<slug>', store_views.ProductDetailAPIView.as_view()),
 ]
 
